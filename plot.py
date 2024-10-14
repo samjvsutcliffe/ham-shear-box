7079acdd20ddc11e10aa3fe89c7de3a003b90340
Sam <sam.sutcliffe2@gmail.com>
2024-10-14 11:19:06 +0100

    Updated plotting of sb

--- plot.py
+++ plot.py
@@ -21,28 +21,46 @@ for f in folders:
     if u not in unique_ids:
         unique_ids.append(u)
 
-unique_ids.sort(key=float)
+#unique_ids.sort(key=float)
 print(unique_ids)
 # unique_ids = ["4"]
 
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colours = prop_cycle.by_key()['color']
+plt.figure(1)
+plt.figure(2)
 
 for colour,unique_id in zip(colours,unique_ids):
     plt.figure(1)
     unreg = re.compile(r'^output-{}-.*'.format(unique_id))
     folders = list(filter(unreg.search,os.listdir("./")))
+    folders.sort(key=lambda x: float(x.split("-")[2]))
     for i in folders:
         print("loading folder: ",i)
         mpm = pd.read_csv("./{}/disp.csv".format(i))
-        l=plt.plot(1e3*mpm["disp"].values,(1e-3/0.06)*mpm["load"].values,label=i,marker=".")
-        maxload = (1e-3/0.06)*mpm["load"].max()
-        maxp=mpm["plastic"].max()
-        maxd=mpm["damage"].max()
-        maxp=0.01e0
-        maxd=1e3
-        plt.plot(1e3*mpm["disp"].values,maxload*mpm["plastic"].values/maxp,label="",marker="x",ls="--",c=l[0].get_color())
-        plt.plot(1e3*mpm["disp"].values,maxload*mpm["damage"].values/maxd,label="",marker="o",ls="--",c=l[0].get_color())
+        if len(mpm["load"]) > 0:
+            mpm["load"] = mpm["load"] - mpm["load"].values[0]
+            l=plt.plot(1e3*mpm["disp"].values,(1e-3/0.06)*mpm["load"].values,label=i,marker=".")
+            maxload = (1e-3/0.06)*mpm["load"].max()
+    plt.xlabel("Displacement (mm)")
+    plt.ylabel("Load (N)")
+    plt.legend()
+
+    plt.figure()
+    plt.title(unique_id)
+    for i in folders:
+        print("loading folder: ",i)
+        mpm = pd.read_csv("./{}/disp.csv".format(i))
+        if len(mpm["load"]) > 0:
+            mpm["load"] = mpm["load"] - mpm["load"].values[0]
+            l=plt.plot(1e3*mpm["disp"].values,(1e-3/0.06)*mpm["load"].values,label=i,marker=".")
+            maxload = 200
+            maxp=mpm["plastic"].max()
+            maxd=mpm["damage"].max()
+            maxp=0.1e0
+            maxd=1e3
+            plt.plot(1e3*mpm["disp"].values,maxload*mpm["plastic"].values/maxp,label="",marker="x",ls="--",c=l[0].get_color())
+            plt.plot(1e3*mpm["disp"].values,maxload*mpm["damage"].values/maxd,label="",marker="o",ls="--",c=l[0].get_color())
     plt.xlabel("Displacement (mm)")
     plt.ylabel("Load (N)")
     plt.legend()
@@ -65,6 +83,7 @@ for colour,unique_id in zip(colours,unique_ids):
         refine,load = extract_vals(f)
         mpm = pd.read_csv("./{}/disp.csv".format(f))
         if len(mpm["load"]) > 0:
+            mpm["load"] = mpm["load"] - mpm["load"].values[0]
             width = 0.06
             p = mpm["load"].max()/width
             r = mpm["load"].values[-1]/width
@@ -79,25 +98,22 @@ for colour,unique_id in zip(colours,unique_ids):
         residual = [x for y, x in sorted(zip(surcharge, residual))]
         surcharge = sorted(surcharge)
 
-        
         m,b = np.polyfit(surcharge, peak, 1)
-        print(m)
+        #unique_id = "D_res = "+unique_id.split("_")[-1]
         plt.scatter(surcharge,peak,label="Peak - {} - {:.2f}, {:.2f}kN".format(unique_id,np.arctan(m)*180/np.pi,b*1e-3),color=colour)
-
         p = plt.plot(surcharge,peak,color=colour)
-
         plt.axline((0,b),slope=m,c=p[0].get_color())
         m,b = np.polyfit(surcharge, residual, 1)
         plt.scatter(surcharge,residual,label="Residual - {} - {:.2f}, {:.2f}kN".format(unique_id,np.arctan(m)*180/np.pi,b*1e-3),color=colour,marker="x")
         r = plt.plot(surcharge,residual,color=colour)
         plt.axline((0,b),slope=m,c=r[0].get_color())
-        
-        
-        
+
         plt.axline((0,0),slope=np.tan(30 * np.pi/180),ls="--")
         plt.axline((0,131e3),slope=np.tan(42 * np.pi/180),ls="--")
         plt.xlim([0,500e3])
         plt.ylim([0,500e3])
+        plt.xlabel("Normal load")
+        plt.xlabel("Shear stress")
         plt.legend()
         plt.savefig("frictional.pdf")
 plt.show()
--- plot_interactive.py
+++ plot_interactive.py
@@ -66,12 +66,12 @@ height = width / 1.618
 
 output_regex = re.compile("output-\d+")
 output_list = list(filter(output_regex.match,os.listdir()))
-output_list.sort(key=lambda x: float(x.split("-")[1]))
+#output_list.sort(key=lambda x: float(x.split("-")[1]))
 print(output_list)
 output_dir = "./{}/".format(output_list[int(input())])
 
 # output_dir = "./ham-shear-box/output-8-1.0e+5/"
-xlim = [0.06,0.12+0.08]
+xlim = [0.03,0.12+0.08]
 ylim = [0,0.10]
 #with open(output_dir+"settings.json") as f:
 #    json_settings = json.load(f)
@@ -90,10 +90,21 @@ plt.close("all")
 files = os.listdir(output_dir)
 finalcsv = re.compile("sim_\d+.vtk")
 files_csvs = list(filter(finalcsv.match,files))
+
+finalpbs = re.compile("sim_pb_\d+.json")
+files_pbs = list(filter(finalpbs.match,files))
+
 framenumber_regex = re.compile("\d+")
 files_csvs = list(map(lambda x: framenumber_regex.findall(x)[0],files_csvs))
 files_csvs.sort(key=float)
 files_csvs = list(map(lambda x: "sim_{}.vtk".format(x), files_csvs))
+
+
+print(files_pbs)
+files_pbs = list(map(lambda x: framenumber_regex.findall(x)[0],files_pbs))
+files_pbs.sort(key=float)
+files_pbs = list(map(lambda x: "sim_pb_{}.json".format(x), files_pbs))
+
 print("files: {}".format(files_csvs))
 dt = 1e4/60
 time = []
@@ -132,12 +143,27 @@ def get_plot(i,fname):
     plt.title("Frame {}".format(i))
     # plt.savefig("outframes/frame_{:05}.png".format(i))
     # plt.clf()
+def plot_pb(i,fname):
+    with open(output_dir + fname) as f:
+        d = json.load(f)
+        for bc in d:
+            # print(bc)
+            p = np.array(bc["POSITION"][:2])
+            normal = np.array(bc["NORMAL"][:2])
+            radius = bc["RADIUS"]
+            normal[0],normal[1] = -normal[1],normal[0]
+            x0 = p - (normal * radius)
+            x1 = p + (normal * radius)
+            plt.plot([x0[0],x1[0]],[x0[1],x1[1]],c="black")
+
+
 
 
 current_frame = 0
 max_frame = len(files_csvs)-1
 def replot():
     get_plot(current_frame,files_csvs[current_frame])
+    plot_pb(current_frame,files_pbs[current_frame])
     fig.canvas.draw()
 
 
@@ -175,7 +201,6 @@ def on_press(event):
     if event.key == 'down':
         current_frame = max(current_frame - 10,0)
         replot()
-    
 
 
 print("Plotting {}, {}",current_frame,files_csvs[current_frame])
