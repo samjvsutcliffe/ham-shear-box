cfd6a816c19c486c5a2f4896b7c439b49952cf03
Sam Sutcliffe <sam.sutcliffe@.dur.ac.uk>
2025-03-07 14:40:18 +0000

    Updated damage-se

--- multi_submit.sh
+++ multi_submit.sh
@@ -4,17 +4,31 @@ module load mvapich2
 module load aocl
 export MV2_ENABLE_AFFINITY=0
 #sbcl --dynamic-space-size 16000  --disable-debugger --load "build_step.lisp" --quit
-rm -r output-*
+#rm -r output-*
 
 
-for ref in 4
+for ref in 8
 do
-    #for l in 100000 125000 150000 175000 200000 225000 250000 275000 300000
+    #for l in 125000 150000 175000 225000 250000 275000
+    #for l in 25000 50000 75000 100000 125000 150000 175000 200000 225000 250000 275000 300000
+    #for l in 50000 100000 150000 200000 250000 300000
     for l in 100000 200000 300000
     do
         export REFINE=$ref
         export LOAD=$l
-        sbatch batch_shear-box.sh
+        sbatch sb-d-se.sh
+    done
+done
+
+for ref in 8
+do
+    #for l in 50000 100000 125000 150000 175000 200000 225000 250000 275000 300000
+    for l in 125000 150000 175000 225000 250000 275000
+    #for l in 100000 200000 300000
+    do
+        export REFINE=$ref
+        export LOAD=$l
+        #sbatch batch_shear-box_big.sh
     done
 done
 
--- plot_paper.py
+++ plot_paper.py
@@ -7,8 +7,6 @@ import numpy as np
 import re
 
 from scipy import integrate
-PDF_OUTPUT = False
-
 
 #plt.rc('font', family='serif', serif='Times')
 ## plt.rc('text', usetex=True)
@@ -24,46 +22,120 @@ plt.rc('axes', labelsize=8)
 width = 3.487
 height = width / 1.618
 
-def calculate_gf(disp,load):
-    return integrate.trapz(load,disp)#/(0.102*0.6*13e-3)
+def extract_vals(f):
+    output,refine,load = f.split("-")
+    #refine = float(refine)
+    return refine,float(load)
 
-data = pd.read_csv("load-disp.csv")
-
-
-print("GF experimental:",calculate_gf(1e0*data["disp"],100e-3*data["load"]))
-
-
-regex = re.compile(r'output-.*')
+regex = re.compile(r'^output-.*')
 folders = list(filter(regex.search,os.listdir("./")))
 
-#plt.figure()
-plt.figure(figsize=(width,height))
-plt.plot(data["disp"],data["load"],label="FEM")
-lower = pd.read_csv("lower.csv")
-upper = pd.read_csv("upper.csv")
-x_min = min(lower["x"].min(),upper["x"].min())
-x_max = max(lower["x"].max(),upper["x"].max())
-x_samples = np.linspace(x_min,x_max,100)
+print(folders)
+unique_ids = []
+for f in folders:
+    u = f.split("-")[1]
+    if u not in unique_ids:
+        unique_ids.append(u)
 
-lower_y = np.interp(x_samples,lower["x"],1e3*lower["y"])
-upper_y = np.interp(x_samples,upper["x"],1e3*upper["y"])
+#unique_ids.sort(key=float)
+print(unique_ids)
+# unique_ids = ["4"]
 
-plt.fill_between(x_samples,lower_y,upper_y, facecolor='grey', alpha=0.5,label="Experimental")
+prop_cycle = plt.rcParams['axes.prop_cycle']
+colours = prop_cycle.by_key()['color']
+plt.figure(1)
+plt.figure(2)
 
-folders = ["./output-0.5-0.0-1.0/",
-           "./output-1.0-0.0-1.0/",
-           "./output-2.0-0.0-1.0/"]
-
-names = ["Coarse","Medium","Fine"]
-for i,name in zip(folders,names):
-    print("loading folder: ",i)
-    mpm = pd.read_csv("./{}/disp.csv".format(i))
-    print("GF mpm:",calculate_gf(1e3*mpm["disp"],100e-3*mpm["load"]))
-    plt.plot(1e3*mpm["disp"],100e-3*mpm["load"],label=name)
+for colour,unique_id in zip(colours,unique_ids):
+    plt.figure(1)
+    unreg = re.compile(r'^output-{}-.*'.format(unique_id))
+    folders = list(filter(unreg.search,os.listdir("./")))
+    folders.sort(key=lambda x: float(x.split("-")[2]))
+    for i in folders:
+        print("loading folder: ",i)
+        mpm = pd.read_csv("./{}/disp.csv".format(i))
+        if len(mpm["load"]) > 0:
+            mpm["load"] = mpm["load"] - mpm["load"].values[0]
+            l=plt.plot(1e3*mpm["disp"].values,(1e-3/0.06)*mpm["load"].values,label=i,marker=".")
+            maxload = (1e-3/0.06)*mpm["load"].max()
     plt.xlabel("Displacement (mm)")
     plt.ylabel("Load (N)")
-plt.tight_layout()
-plt.legend()
-#plt.legend(["FEM","Experimental","Coarse","Medium","Fine"])
-plt.savefig("paper.pdf")
+    plt.legend()
+
+    plt.figure()
+    plt.title(unique_id)
+    for i in folders:
+        print("loading folder: ",i)
+        mpm = pd.read_csv("./{}/disp.csv".format(i))
+        if len(mpm["load"]) > 0:
+            mpm["load"] = mpm["load"] - mpm["load"].values[0]
+            l=plt.plot(1e3*mpm["disp"].values,(1e-3/0.06)*mpm["load"].values,label=i,marker=".")
+            maxload = 200
+            maxp=mpm["plastic"].max()
+            maxd=mpm["damage"].max()
+            maxp=0.1e0
+            maxd=1e1
+            maxe=1e0
+            #plt.plot(1e3*mpm["disp"].values,mpm["plastic"].values/maxp,label="",marker="x",ls="--",c=l[0].get_color())
+            #plt.plot(1e3*mpm["disp"].values,mpm["damage"].values/maxd,label="",marker="o",ls="--",c=l[0].get_color())
+            #plt.plot(1e3*mpm["disp"].values,mpm["energy"].values/maxe,label="",marker="*",ls="--",c=l[0].get_color())
+    plt.xlabel("Displacement (mm)")
+    plt.ylabel("Load (N)")
+    plt.legend()
+    plt.savefig("load-disp.pdf")
+    # plt.figure()
+    # for i in folders:
+    #     print("loading folder: ",i)
+    #     mpm = pd.read_csv("./{}/disp.csv".format(i))
+    #     plt.plot(1e3*mpm["disp"].values,(1e-3/0.06)*mpm["load"].values,label=i,marker=".")
+    # plt.xlabel("Displacement (mm)")
+    # plt.ylabel("Load (N)")
+    # plt.legend()
+    # plt.savefig("load-disp-{}.pdf".format(unique_id))
+    
+    surcharge = []
+    peak = []
+    residual = []
+    plt.figure(2)
+    for f in folders:
+        refine,load = extract_vals(f)
+        mpm = pd.read_csv("./{}/disp.csv".format(f))
+        if len(mpm["load"]) > 0:
+            mpm["load"] = mpm["load"] - mpm["load"].values[0]
+            width = 0.06
+            residual_window = 0.5
+            residual_end = round(len(mpm["load"]) * (1 - residual_window))
+            p = mpm["load"].max()/width
+            residual_window = 0.5
+            residual_back = round(len(mpm["load"].values) * (1 - residual_window))
+            r = mpm["load"].values[residual_back:].mean()/width
+            surcharge.append(load)
+            peak.append(p)
+            residual.append(r)
+            # plt.scatter(load,r)
+            # plt.scatter(load,p)
+
+    if len(peak) > 0:
+        peak = [x for y, x in sorted(zip(surcharge, peak))]
+        residual = [x for y, x in sorted(zip(surcharge, residual))]
+        surcharge = sorted(surcharge)
+
+        m,b = np.polyfit(surcharge, peak, 1)
+        #unique_id = "D_res = "+unique_id.split("_")[-1]
+        plt.scatter(surcharge,peak,label="Peak - {} - {:.2f}, {:.2f}kN".format(unique_id,np.arctan(m)*180/np.pi,b*1e-3),color=colour)
+        p = plt.plot(surcharge,peak,color=colour)
+        plt.axline((0,b),slope=m,c=p[0].get_color())
+        m,b = np.polyfit(surcharge, residual, 1)
+        plt.scatter(surcharge,residual,label="Residual - {} - {:.2f}, {:.2f}kN".format(unique_id,np.arctan(m)*180/np.pi,b*1e-3),color=colour,marker="x")
+        r = plt.plot(surcharge,residual,color=colour)
+        plt.axline((0,b),slope=m,c=r[0].get_color())
+
+        plt.axline((0,0),slope=np.tan(30 * np.pi/180),ls="--")
+        plt.axline((0,131e3),slope=np.tan(42 * np.pi/180),ls="--")
+        plt.xlim([0,500e3])
+        plt.ylim([0,500e3])
+        plt.xlabel("Normal load")
+        plt.ylabel("Shear stress")
+        plt.legend()
+        plt.savefig("frictional.pdf")
 plt.show()
